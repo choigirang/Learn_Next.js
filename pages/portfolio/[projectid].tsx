9f0c327a18b901c1d5fdb1f83d38c1bcf07c0af7
GiRang Choi <choigirang@kakao.com>
2023-05-02 00:17:07 +0900

    feat: 동적 라우팅

--- pages/portfolio/[projectid].tsx
+++ pages/portfolio/[projectid].tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { useRouter, withRouter } from "next/router";
+// 클래스 기반 컴포넌트를 묶어 특수한 라우터 프로퍼티를 컴포넌트에 주입하는 역할
+// 하지만 주로 함수형 컴포넌트를 사용하기 때문에 쓰이지 않는다.
 
 export default function PortfolioProjectPage() {
+  const router = useRouter();
+
+  router.pathname;
+  router.query;
+
   return <div>[projectid]</div>;
 }
