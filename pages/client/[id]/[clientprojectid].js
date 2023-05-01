251ee63f45f423dbcda170e0a05efdc91997894e
GiRang Choi <choigirang@kakao.com>
2023-05-02 00:17:16 +0900

    feat: 중첩된 동적 라우팅

---
+++ pages/client/[id]/[clientprojectid].js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useRouter } from 'next/router';
+
+export default function SelectedClientProjectPage() {
+
+    const router = useRouter();
+    console.log(router.query)
+
+    return <div>SelectedClientProjectPage</div>;
+}
